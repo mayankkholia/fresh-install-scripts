8c8ef6faea64b370d5f64c7bd4f501a99a68832d
Mayank Kholia <mkholia96@gmail.com>
2020-08-30 10:59:37 +0530

    Added Docker,Rustscan

--- bug-bounty-tools.sh
+++ bug-bounty-tools.sh
@@ -1,3 +1,11 @@
+##Update Repositories and install Docker
+sudo apt update
+sudo apt install -y docker.io
+sudo systemctl enable docker --now
+
+## GitTools -- Dumping Git Repositories
 cd /opt
 git clone https://github.com/internetwache/GitTools.git
 
+## Rustscan -- Faster Nmap
+sudo docker pull rustscan/rustscan:alpine
